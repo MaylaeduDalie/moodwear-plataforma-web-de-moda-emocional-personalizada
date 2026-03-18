9f788c2b08fe978574594a2fe339d63aa6b334c1
MaylaeduDalie <mayla.vieira@estudante.ifto.edu.br>
2026-03-18 08:11:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 console.log("O JavaScript está funcionando!");
 
-const botao = document.getElementById("favoritar");
+const botaoGerar = document.getElementById("gerar");
+const humor = document.getElementById("humor");
+const texto = document.getElementById("texto-resultado");
 
-botao.addEventListener("click", function() {
-    botao.textContent = "✔ Favoritado!";
+botaoGerar.addEventListener("click", function () {
+
+    if (humor.value === "feliz") {
+        texto.textContent = "Looks leves e coloridos para combinar com sua felicidade!";
+    } else if (humor.value === "calmo") {
+        texto.textContent = "Tons suaves e neutros para manter sua tranquilidade.";
+    } else if (humor.value === "confianca") {
+        texto.textContent = "Peças elegantes para destacar sua confiança.";
+    } else {
+        texto.textContent = "Um look especial baseado no seu humor!";
+    }
+
+});
+
+const botaoFav = document.getElementById("favoritar");
+
+botaoFav.addEventListener("click", function () {
+    botaoFav.textContent = "✔ Favoritado!";
+    botaoFav.style.backgroundColor = "#2ecc71";
 });
